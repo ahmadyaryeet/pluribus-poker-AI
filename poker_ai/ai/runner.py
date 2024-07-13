2586951043f4d31540e9f0a5bb1b0a69a1bd303f
ahmadyaryeet <saroyaahmad@gmail.com>
2024-07-14 00:17:40 +0100

    h

--- poker_ai/ai/runner.py
+++ poker_ai/ai/runner.py
@@ -122,7 +122,7 @@ def resume(server_config_path: str):
 @click.option(
     "--low_card_rank",
     #default=2,
-    default=4,
+    default=2,
     help=(
         "The starting hand rank from 2 through 14 for the deck we want to "
         "cluster. We recommend starting small."
